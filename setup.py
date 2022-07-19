0cea9561d0b0620b64a76363cecec1e837b4d185
Jack Ceroni <jackceroni@gmail.com>
2022-07-19 12:20:08 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tailgating',
+    version='0.0.1',
+    packages=['tailgating'],
+    url='',
+    license='',
+    author='jackceroni',
+    author_email='jackceroni@gmail.com',
+    description='Tailgating quantum circuits',
+)
